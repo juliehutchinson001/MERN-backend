2b36e6bf65dbdf8b6e848eb871769d219758ba56
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-01-10 10:29:11 -0800

    feat: request geolocation response of an address

--- playground/mongodb-connection.js
+++ playground/mongodb-connection.js
@@ -1,9 +1,12 @@
 const request = require('request');
+const secretKey = require('../middleware/secret-keys');
+
+const googleKey = secretKey.googleMapKey;
+const location = '2859%20ne4th%20street%20florida';
 
 request(
   {
-    url:
-      'http://www.mapquestapi.com/geocoding/v1/address?key=zubofZfHuhsCmRNSpU8OCerCezwAeXDf&location=2859%20ne4th%20street%20florida',
+    url: `http://www.mapquestapi.com/geocoding/v1/address?key=${googleKey}&location=${location}`,
     json: true,
   },
   (error, response, body) => {

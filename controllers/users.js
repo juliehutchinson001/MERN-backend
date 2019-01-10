6d6e2f8653261a7f5bfcad65f28db5a79b760453
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-01-09 18:04:13 -0800

    feat: create route to my user

--- controllers/users.js
+++ controllers/users.js
@@ -2,3 +2,29 @@ const express = require('express');
 const User = require('../models/User');
 const authenticate = require('../middleware/authenticate');
 
+const router = express.Router();
+
+router.post('/', (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+
+  const newUser = new User({
+    firstName,
+    lastName,
+    userId: firstName + lastName,
+    email,
+    password,
+	});
+
+	newUser
+    .save()
+  		.then(() => newUser.generateAuthToken())
+      .then(token => res.header('x-auth', token).json({ firstName, lastName }))
+      .catch(error => res.status(400).json({ error }));
+});
+
+router.get('/me', authenticate, (req, res) => {
+  const { firstName, lastName } = req.user;
+  res.json({ firstName, lastName });
+});
+
+module.exports = router;

9fcc2cdafe33a1cad24414b41979f2316cea0dec
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-01-09 18:04:59 -0800

    feat: code to authenticate token of a user

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,2 +1,19 @@
 const User = require('../models/User');
 
+const authenticate = (req, res, next) => {
+  const token = req.header('x-auth');
+
+  User.findByToken(token)
+  .then(user => {
+    if(!user) {
+      return Promise.reject();
+    }
+
+    req.user = user;
+    req.token = token;
+    next();
+  })
+  .catch(error => res.status(401).json({ error }))
+};
+
+module.exports = authenticate;

d6e9b66f1fa0277df72427b6b0f392260a2b696b
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-01-09 21:05:55 -0800

    fix: changing trailing comma setting eslint

---
+++ models/user.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+
+const UserSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    minlength: 2,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    minlength: 2,
+  },
+  userId: {
+    type: String,
+    required: true,
+    minlength: 2,
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 2,
+    trim: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  tokens:[{
+    access: {
+      type: String,
+      required: true,
+    },
+    token: {
+      type: String,
+      required: true,
+    },
+  }],
+
+});
+
+UserSchema.methods.generateAuthToken = function generateAuthToken() {
+  const access = 'auth';
+  const token = jwt.sign({ _id: this._id.toHexString(), access }, 'secret')
+    .toString();
+
+}
--- playground/mongodb-connection.js
+++ playground/mongodb-connection.js
@@ -1,1 +1,9 @@
-const { MongoClient } = require('mongodb');
+const aString = 'THIS HAS DOUBLE QUOTES';
+
+const obj = {
+  name: 'Jul',
+  age: 29,
+  city: 'California',
+};
+
+console.log(`${aString}, ${obj}`);

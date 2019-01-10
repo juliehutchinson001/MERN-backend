b48d71d6e5ce3a8f7124a2865d63303f661b9d0d
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-01-10 09:59:13 -0800

    refactor: add secret key file to ignored paths

--- playground/mongodb-connection.js
+++ playground/mongodb-connection.js
@@ -1,9 +1,12 @@
-const aString = 'THIS HAS DOUBLE QUOTES';
+const request = require('request');
 
-const obj = {
-  name: 'Jul',
-  age: 29,
-  city: 'California',
-};
-
-console.log(`${aString}, ${obj}`);
+request(
+  {
+    url:
+      'http://www.mapquestapi.com/geocoding/v1/address?key=zubofZfHuhsCmRNSpU8OCerCezwAeXDf&location=2859%20ne4th%20street%20florida',
+    json: true,
+  },
+  (error, response, body) => {
+    console.log(JSON.stringify(response, null, 2));
+  }
+);

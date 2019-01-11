bdefa4335889e683e9b1dad75d97c632a9a4a43b
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-01-10 18:56:28 -0800

    feat: example of using mongoose

---
+++ playground/mongoose-config.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const url = 'mongodb://localhost:27017/TodoApp';
+mongoose.connect(
+  url,
+  { useNewUrlParser: true }
+);
+
+const Interest = mongoose.model('Interests', {
+  text: { type: String, required: true, trim: true, minLength: 5 },
+  completed: { type: Boolean, trim: true, default: false },
+  completedAt: { type: Number },
+});
+
+const newInterest = new Interest({ text: 'Learn Node' });
+newInterest.save().then(
+  res => {
+    console.log(`Saved Interest: ${res}`);
+  },
+  err => {
+    console.log('Unable to save new interest');
+  }
+);

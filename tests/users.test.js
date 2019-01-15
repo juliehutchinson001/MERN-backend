5d4d73d3648084d21243e1a68e9640edcd5dffb4
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-01-14 17:30:32 -0800

    feat: run first test to verify connection with node works

--- tests/users.test.js
+++ tests/users.test.js
@@ -3,3 +3,19 @@ const app = require('../app');
 const { connectMongoose, disconnectMongoose } = require('../db/mongoose');
 const { users, seedUsersToDB } = require('./seed');
 
+const fakeUser = {
+  firstName: 'fake',
+  lastName: 'user',
+  email: 'fake@test.com',
+  password: 'fakeuserpassword',
+};
+
+
+
+/*  it('Basic test works', async () => {
+    expect.assertions(1);
+    // Async way
+    const response = await request(app).get('/api/users/test');
+    expect(response.body.msg).toBe('it works');
+  });
+*/

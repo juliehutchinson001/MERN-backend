b7b101b574001f14be68fa78a7836c7fd6df3a25
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-02-21 21:49:06 -0800

    fix: add config fix

--- config/config.js
+++ config/config.js
@@ -2,3 +2,13 @@
 const currentEnvironment = process.env.NODE_ENV || 'development';
 const secretKeys = require('./secret-keys');
 
+// Sets env variables when not in production
+module.exports = () => {
+  if (currentEnvironment === 'development' || currentEnvironment === 'test') {
+    const environmentVars = secretKeys[currentEnvironment];
+
+    Object.keys(environmentVars).forEach(envVar => {
+      process.env[envVar] = environmentVars[envVar];
+    });
+  }
+};

937d8afecdbbe535923f92a916bc7c5195fc4c44
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-02-24 10:27:43 -0800

    init: add authentication

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,19 +1,7 @@
-const User = require('../models/user');
+const { User } = require('../models/user');
 
-const authenticate = (req, res, next) => {
-  const token = req.header('x-auth');
-
-  User.findByToken(token)
-    .then(user => {
-      if (!user) {
-        return Promise.reject();
-      }
-
-      req.user = user;
-      req.token = token;
-      next();
-    })
-    .catch(error => res.status(401).json({ error }));
+const authenticate = (request, response, next) => {
+  
 };
 
 module.exports = authenticate;

5d9dabc0abe0c99b9331fb4450ddefa0dec6b81d
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-02-25 16:25:02 -0800

    add authentication to input receiveed

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,7 +1,19 @@
 const { User } = require('../models/user');
 
 const authenticate = (request, response, next) => {
-  
+  const token = request.header('x-auth');
+
+  User.findByToken(token)
+    .then(user => {
+      if (!user) {
+        return Promise.reject();
+      }
+
+      request.user = user;
+      request.token = token;
+      next();
+    })
+    .catch(error => response.status(401).json({ error }));
 };
 
 module.exports = authenticate;

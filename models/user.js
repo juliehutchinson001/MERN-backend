32b3f7b102fbf4eb7cb781b6bb5ec57e575fa338
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-03-02 10:14:56 -0800

    add functionality to user's schema

--- models/user.js
+++ models/user.js
@@ -45,3 +45,25 @@ const UserSchema = new mongoose.Schema({
     },
   ],
 });
+
+UserSchema.pre('save', function hashPasswordBeforeSave(next) {
+  if (this.isModified('password')) {
+    bcrypt.genSalt((saltError, salt) => {
+      if (saltError) {
+        return Promise.reject();
+      }
+      bcrypt.hash(this.password, salt, (hashError, hash) => {
+        if (hashError) {
+          return Promise.reject();
+        }
+        this.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+
+module.exports = { User: mongoose.model('users', UserSchema) };

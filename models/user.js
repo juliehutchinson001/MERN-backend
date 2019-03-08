fec8e64ef898df9e2dad213d932457991677e553
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-03-08 07:44:21 -0800

    add tokens to user schema

--- models/user.js
+++ models/user.js
@@ -38,7 +38,10 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true,
       },
-      
+      token: {
+        type: String,
+        required: true,
+      },
     },
   ],
 });
